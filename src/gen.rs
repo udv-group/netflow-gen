7b581943a437f557a43d97c04d46dc6c05c690af
Mikhail Antoshkin <51194616+mikhailantoshkin@users.noreply.github.com>
2025-05-06 10:02:56 +0900

    chore: bump deps, update runners (#2)
    * chore: bump deps
    
    * update runners
    
    * runners

--- src/gen.rs
+++ src/gen.rs
@@ -1,11 +1,11 @@
 use chrono::Utc;
 use core::ops::RangeInclusive;
 use ipnet::Ipv4Net;
-use std::{cell::RefCell, collections::HashMap, fmt::Display, rc::Rc};
+use std::{collections::HashMap, fmt::Display};
 
 use netgauze_flow_pkt::{
     ie,
-    netflow::{DataRecord, NetFlowV9Packet, Set, TemplateRecord, TemplatesMap},
+    netflow::{DataRecord, DecodingTemplate, NetFlowV9Packet, Set, TemplateRecord, TemplatesMap},
     DataSetId, FieldSpecifier,
 };
 use rand::rngs::ThreadRng;
@@ -48,9 +48,12 @@ pub enum Protocol {
     TCP = 6,
     ICMP = 1,
 }
-impl From<Protocol> for u8 {
+impl From<Protocol> for ie::protocolIdentifier {
     fn from(value: Protocol) -> Self {
-        value as u8
+        match value {
+            Protocol::TCP => ie::protocolIdentifier::TCP,
+            Protocol::ICMP => ie::protocolIdentifier::ICMP,
+        }
     }
 }
 impl TryFrom<&str> for Protocol {
@@ -156,11 +159,13 @@ impl NetFlowGenBuilder {
         ];
         let scope_filed_specifiers = vec![];
 
-        let map = HashMap::from([(
+        let templates_map: TemplatesMap = HashMap::from([(
             template_id,
-            Rc::new((scope_filed_specifiers, field_specifiers.clone())),
+            DecodingTemplate::new(
+                scope_filed_specifiers.into(),
+                field_specifiers.clone().into(),
+            ),
         )]);
-        let templates_map: TemplatesMap = Rc::new(RefCell::new(map));
 
         let gen = NetFlowGenerator {
             src: self.src,
@@ -172,8 +177,8 @@ impl NetFlowGenBuilder {
             dst_mac: self.dst_mac,
             seq: 1,
             template_id,
-            rng: rand::thread_rng(),
-            template: TemplateRecord::new(template_id, field_specifiers),
+            rng: rand::rng(),
+            template: TemplateRecord::new(template_id, field_specifiers.into()),
             data_records_per_packet: 1,
             sync_stategy: strat,
         };
@@ -202,24 +207,21 @@ impl<T> NetFlowGenerator<T> {
         (0..self.data_records_per_packet)
             .map(|_| {
                 DataRecord::new(
-                    vec![],
+                    vec![].into(),
                     vec![
-                        ie::Field::sourceIPv4Address(ie::sourceIPv4Address(
-                            self.src.get_one(&mut self.rng),
-                        )),
-                        ie::Field::destinationIPv4Address(ie::destinationIPv4Address(
-                            self.dst.get_one(&mut self.rng),
-                        )),
-                        ie::Field::sourceTransportPort(ie::sourceTransportPort(
+                        ie::Field::sourceIPv4Address(self.src.get_one(&mut self.rng)),
+                        ie::Field::destinationIPv4Address(self.dst.get_one(&mut self.rng)),
+                        ie::Field::sourceTransportPort(
                             self.src_port.clone().choose(&mut self.rng).unwrap(),
-                        )),
-                        ie::Field::destinationTransportPort(ie::destinationTransportPort(
+                        ),
+                        ie::Field::destinationTransportPort(
                             self.dst_port.clone().choose(&mut self.rng).unwrap(),
-                        )),
-                        ie::Field::sourceMacAddress(ie::sourceMacAddress(self.src_mac)),
-                        ie::Field::destinationMacAddress(ie::destinationMacAddress(self.dst_mac)),
-                        ie::Field::protocolIdentifier(ie::protocolIdentifier(self.protocol.into())),
-                    ],
+                        ),
+                        ie::Field::sourceMacAddress(self.src_mac),
+                        ie::Field::destinationMacAddress(self.dst_mac),
+                        ie::Field::protocolIdentifier(self.protocol.into()),
+                    ]
+                    .into(),
                 )
             })
             .collect()
@@ -230,7 +232,7 @@ impl<T> NetFlowGenerator<T> {
             Utc::now(),
             self.seq,
             0,
-            vec![Set::Template(vec![self.template.clone()])],
+            vec![Set::Template(vec![self.template.clone()].into())].into(),
         )
     }
     fn data_packet(&mut self) -> NetFlowV9Packet {
@@ -241,8 +243,9 @@ impl<T> NetFlowGenerator<T> {
             0,
             vec![Set::Data {
                 id: DataSetId::new(self.template_id).unwrap(),
-                records: self.data_records(),
-            }],
+                records: self.data_records().into(),
+            }]
+            .into(),
         )
     }
     fn mixed_packet(&mut self) -> NetFlowV9Packet {
@@ -252,12 +255,13 @@ impl<T> NetFlowGenerator<T> {
             self.seq,
             0,
             vec![
-                Set::Template(vec![self.template.clone()]),
+                Set::Template(vec![self.template.clone()].into()),
                 Set::Data {
                     id: DataSetId::new(self.template_id).unwrap(),
-                    records: self.data_records(),
+                    records: self.data_records().into(),
                 },
-            ],
+            ]
+            .into(),
         )
     }
 }
